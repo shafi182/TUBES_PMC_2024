64f36f0e021e61adb727f84409ff0e9dd53834ce
Muhammad Shafiyuddin Mihradi <134825946+shafi182@users.noreply.github.com>
2024-06-06 23:24:05 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+
+#include "variable.h"
+#include "reader.c"
+
+
+int main(){    
+    dataPasienHead = (dataPasien*) malloc(sizeof(dataPasien));
+    riwayatDiagnosisHead = (riwayatDiagnosis*) malloc(sizeof(riwayatDiagnosis));
+
+    readFile(&dataPasienHead, &riwayatDiagnosisHead);
+
+    riwayatDiagnosis *cur = riwayatDiagnosisHead;
+    for(i=0; i<3; i++){
+        cur = cur->next;
+    }
+    printf("%d %s %s\n", riwayatDiagnosisHead->biaya, riwayatDiagnosisHead->idPasien, riwayatDiagnosisHead->diagnosis);
+    return 0;
+}
